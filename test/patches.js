c3114bbcc0e87f57eae98697dd1e8007b01f7298
Aaron Jorbin <aaron@jorb.in>
2018-03-09 02:53:38 -0500

    fix formatting of test/patches.js

--- test/patches.js
+++ test/patches.js
@@ -1,115 +1,115 @@
-var grunt = require( 'grunt' )
-	, expect = require( 'chai' ).expect
-	, patch = require( '../lib/patch.js' )
-	, coreGit = grunt.file.read ( 'test/fixtures/core.git.diff' )
-	, coreSvn = grunt.file.read ( 'test/fixtures/core.svn.diff' )
-	, developGit = grunt.file.read ( 'test/fixtures/develop.git.diff' )
-	, developSvn = grunt.file.read ( 'test/fixtures/develop.svn.diff' )
-	, coreIndexGit = grunt.file.read ( 'test/fixtures/core.git.index.diff' )
-	, coreIndexSvn = grunt.file.read ( 'test/fixtures/core.svn.index.diff' )
-	, developIndexGit = grunt.file.read ( 'test/fixtures/develop.git.index.diff' )
-	, developIndexSvn = grunt.file.read ( 'test/fixtures/develop.svn.index.diff' )
-	, developSampleGit = grunt.file.read ( 'test/fixtures/develop.git.wp-config-sample.diff' )
-	, developSampleSvn = grunt.file.read ( 'test/fixtures/develop.svn.wp-config-sample.diff' )
-	, testsSvn = grunt.file.read ( 'test/fixtures/tests.develop.svn.diff' )
-	, testsGit = grunt.file.read ( 'test/fixtures/tests.develop.git.diff' )
-	, abyes = grunt.file.read( 'test/fixtures/git.diff.ab.diff' )
-	, coreTrunkSvn = grunt.file.read ( 'test/fixtures/core.svn.trunk.diff' )
+var grunt = require( 'grunt' ),
+	expect = require( 'chai' ).expect,
+	patch = require( '../lib/patch.js' ),
+	coreGit = grunt.file.read ( 'test/fixtures/core.git.diff' ),
+	coreSvn = grunt.file.read ( 'test/fixtures/core.svn.diff' ),
+	developGit = grunt.file.read ( 'test/fixtures/develop.git.diff' ),
+	developSvn = grunt.file.read ( 'test/fixtures/develop.svn.diff' ),
+	coreIndexGit = grunt.file.read ( 'test/fixtures/core.git.index.diff' ),
+	coreIndexSvn = grunt.file.read ( 'test/fixtures/core.svn.index.diff' ),
+	developIndexGit = grunt.file.read ( 'test/fixtures/develop.git.index.diff' ),
+	developIndexSvn = grunt.file.read ( 'test/fixtures/develop.svn.index.diff' ),
+	developSampleGit = grunt.file.read ( 'test/fixtures/develop.git.wp-config-sample.diff' ),
+	developSampleSvn = grunt.file.read ( 'test/fixtures/develop.svn.wp-config-sample.diff' ),
+	testsSvn = grunt.file.read ( 'test/fixtures/tests.develop.svn.diff' ),
+	testsGit = grunt.file.read ( 'test/fixtures/tests.develop.git.diff' ),
+	abyes = grunt.file.read( 'test/fixtures/git.diff.ab.diff' ),
+	coreTrunkSvn = grunt.file.read ( 'test/fixtures/core.svn.trunk.diff' );
 
 describe( 'Patch helpers', function() {
 
-	it( 'git a/b diffs should not automatticaly trigger moving to src', function( done ){
-		expect( patch.move_to_src( abyes ) ).to.not.be.true
+	it( 'git a/b diffs should not automatticaly trigger moving to src', function( done ) {
+		expect( patch.move_to_src( abyes ) ).to.not.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'tests diffs should always be applied in the root of the checkout', function(done){
+	it( 'tests diffs should always be applied in the root of the checkout', function( done ) {
 
-		expect( patch.move_to_src( testsGit ) ).to.not.be.true
-		expect( patch.move_to_src( testsSvn ) ).to.not.be.true
+		expect( patch.move_to_src( testsGit ) ).to.not.be.true;
+		expect( patch.move_to_src( testsSvn ) ).to.not.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'dev.git diffs should always be applied in the root of the checkout', function(done){
+	it( 'dev.git diffs should always be applied in the root of the checkout', function( done ) {
 
-		expect( patch.move_to_src( developGit ) ).to.not.be.true
-		expect( patch.move_to_src( developIndexGit ) ).to.not.be.true
+		expect( patch.move_to_src( developGit ) ).to.not.be.true;
+		expect( patch.move_to_src( developIndexGit ) ).to.not.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'dev.svn diffs should always be applied in the root of the checkout', function(done){
+	it( 'dev.svn diffs should always be applied in the root of the checkout', function( done ) {
 
-		expect( patch.move_to_src( developSvn ) ).to.not.be.true
-		expect( patch.move_to_src( developIndexSvn ) ).to.not.be.true
+		expect( patch.move_to_src( developSvn ) ).to.not.be.true;
+		expect( patch.move_to_src( developIndexSvn ) ).to.not.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'core.git.wordpress.org diffs should always be applied in the svn folder', function(done){
+	it( 'core.git.wordpress.org diffs should always be applied in the svn folder', function( done ) {
 
-		expect( patch.move_to_src( coreGit ) ).to.be.true
+		expect( patch.move_to_src( coreGit ) ).to.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'core.svn.wordpress.org diffs should always be applied in the svn folder', function(done){
+	it( 'core.svn.wordpress.org diffs should always be applied in the svn folder', function( done ) {
 
-		expect( patch.move_to_src( coreSvn ) ).to.be.true
+		expect( patch.move_to_src( coreSvn ) ).to.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'core.svn.wordpress.org diffs from trunk should always be applied in the src folder', function(done){
+	it( 'core.svn.wordpress.org diffs from trunk should always be applied in the src folder', function( done ) {
 
-		expect( patch.move_to_src( coreTrunkSvn ) ).to.be.true
-		expect( patch.is_ab( coreTrunkSvn ) ).to.be.true
+		expect( patch.move_to_src( coreTrunkSvn ) ).to.be.true;
+		expect( patch.is_ab( coreTrunkSvn ) ).to.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it( 'index.php should always be applied in the src folder', function(done){
+	it( 'index.php should always be applied in the src folder', function( done ) {
 
-		expect( patch.move_to_src( coreIndexSvn ) ).to.be.true
-		expect( patch.move_to_src( coreIndexGit ) ).to.be.true
+		expect( patch.move_to_src( coreIndexSvn ) ).to.be.true;
+		expect( patch.move_to_src( coreIndexGit ) ).to.be.true;
 
-		done()
+		done();
 
-	})
+	});
 
-	it( 'wp-config-sample.php should always be applied in the root folder', function(done){
-		expect( patch.move_to_src( developSampleSvn ) ).to.not.be.true
-		expect( patch.move_to_src( developSampleGit ) ).to.not.be.true
+	it( 'wp-config-sample.php should always be applied in the root folder', function( done ) {
+		expect( patch.move_to_src( developSampleSvn ) ).to.not.be.true;
+		expect( patch.move_to_src( developSampleGit ) ).to.not.be.true;
 
-		done()
+		done();
 
-	})
+	});
 
-	it ( 'is_ab should return true on patches with a/ b/ style', function(done){
+	it ( 'is_ab should return true on patches with a/ b/ style', function( done ) {
 
-		expect( patch.is_ab( abyes ) ).to.be.true
+		expect( patch.is_ab( abyes ) ).to.be.true;
 
-		done()
-	})
+		done();
+	});
 
-	it ( 'is_ab should return false on patches without a/ b/ style', function(done){
+	it ( 'is_ab should return false on patches without a/ b/ style', function( done ) {
 
-		expect( patch.is_ab( developSampleGit ) ).to.not.be.true
-		expect( patch.is_ab( developSampleSvn ) ).to.not.be.true
-		expect( patch.is_ab( coreIndexGit ) ).to.not.be.true
-		expect( patch.is_ab( coreIndexSvn ) ).to.not.be.true
-		expect( patch.is_ab( coreGit ) ).to.not.be.true
-		expect( patch.is_ab( coreSvn ) ).to.not.be.true
-		expect( patch.is_ab( developGit ) ).to.not.be.true
-		expect( patch.is_ab( developSvn ) ).to.not.be.true
-		expect( patch.is_ab( developIndexGit ) ).to.not.be.true
-		expect( patch.is_ab( developIndexSvn ) ).to.not.be.true
-		expect( patch.is_ab( testsGit ) ).to.not.be.true
-		expect( patch.is_ab( testsSvn ) ).to.not.be.true
+		expect( patch.is_ab( developSampleGit ) ).to.not.be.true;
+		expect( patch.is_ab( developSampleSvn ) ).to.not.be.true;
+		expect( patch.is_ab( coreIndexGit ) ).to.not.be.true;
+		expect( patch.is_ab( coreIndexSvn ) ).to.not.be.true;
+		expect( patch.is_ab( coreGit ) ).to.not.be.true;
+		expect( patch.is_ab( coreSvn ) ).to.not.be.true;
+		expect( patch.is_ab( developGit ) ).to.not.be.true;
+		expect( patch.is_ab( developSvn ) ).to.not.be.true;
+		expect( patch.is_ab( developIndexGit ) ).to.not.be.true;
+		expect( patch.is_ab( developIndexSvn ) ).to.not.be.true;
+		expect( patch.is_ab( testsGit ) ).to.not.be.true;
+		expect( patch.is_ab( testsSvn ) ).to.not.be.true;
 
-		done()
-	})
+		done();
+	});
 
-})
+});
